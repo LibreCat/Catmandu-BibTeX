af53599e67b75c3772e62e57e776fd6fe63ddd28
vpeil <vitali.peil@uni-bielefeld.de>
2013-06-21 15:41:44 +0200

    added Catmandu::Importer::BibTeX and tests; status: working

--- lib/Catmandu/Importer/BibTeX.pm
+++ lib/Catmandu/Importer/BibTeX.pm
@@ -3,20 +3,27 @@ package Catmandu::Importer::BibTeX;
 use namespace::clean;
 use Catmandu::Sane;
 use BibTeX::Parser;
-use LaTeX::Encode;
 use Moo;
 
 with 'Catmandu::Importer';
 
-has bibtex => (is => 'ro', lazy => 1, builder => '_build_bibtex');
-
-
-sub _build_bibtex {
-
-}
-
 sub generator {
-
+    my ($self) =@_;
+    my $fh = $self->fh;
+    sub {
+        my $parser = BibTeX::Parser->new($fh);
+        while (my $entry = $parser->next ) {
+            my $bib = {};
+            if ($entry->parse_ok) {
+                $bib->{type} = $entry->type;
+                map {$bib->{$_} = $entry->field($_)} $entry->fieldlist;
+                return $bib;
+            } else {
+                Catmandu::Error->throw($entry->error);
+            }
+        }
+        return;
+    }
 }
 
 =head1 NAME
@@ -29,7 +36,10 @@ Catmandu::Importer::BibTeX - a BibTeX importer
 
     my $importer = Catmandu::Importer::BibTeX->new(file => "/foo/bar.bib");
 
-    
+    my $n = $importer->each(sub {
+        my $hashref = $_[0];
+        # ...
+    }); 
 
 =head1 DESCRIPTION
 
