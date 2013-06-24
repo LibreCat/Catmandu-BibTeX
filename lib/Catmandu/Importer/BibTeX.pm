576879933362007be075e92f8db7a77f5263e279
vpeil <vitali.peil@uni-bielefeld.de>
2013-06-24 07:40:45 +0200

    decoding added

--- lib/Catmandu/Importer/BibTeX.pm
+++ lib/Catmandu/Importer/BibTeX.pm
@@ -3,20 +3,30 @@ package Catmandu::Importer::BibTeX;
 use namespace::clean;
 use Catmandu::Sane;
 use BibTeX::Parser;
+use LaTeX::Decode;
 use Moo;
 
 with 'Catmandu::Importer';
 
+has decode => (is => 'ro', default => sub {0});
+
 sub generator {
     my ($self) =@_;
     my $fh = $self->fh;
+    my $decode = $self->decode;
     sub {
         my $parser = BibTeX::Parser->new($fh);
         while (my $entry = $parser->next ) {
             my $bib = {};
             if ($entry->parse_ok) {
-                $bib->{type} = $entry->type;
-                map {$bib->{$_} = $entry->field($_)} $entry->fieldlist;
+                $bib->{type} = lc $entry->type;
+                if ($decode) {
+                    map { $bib->{$_} = latex_decode($entry->field($_)) } 
+                        $entry->fieldlist;
+                } else {
+                    map { $bib->{$_} = $entry->field($_) } 
+                        $entry->fieldlist;
+                }
                 return $bib;
             } else {
                 Catmandu::Error->throw($entry->error);
@@ -41,7 +51,19 @@ Catmandu::Importer::BibTeX - a BibTeX importer
         # ...
     }); 
 
-=head1 DESCRIPTION
+=head1 METHODS
+
+=head2 new([file => $filename])
+
+Create a new BibTeX importer for $filename. Use STDIN when no filename is given.
+
+=head2 count
+
+=head2 each(&callback)
+
+=head2 ...
+
+Every Catmandu::Importer is a Catmandu::Iterable all its methods are inherited.
 
 =head1 SEE ALSO
 
